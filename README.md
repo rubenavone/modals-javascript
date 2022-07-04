3e7d462b8e1985717d6c566d902374c4985ba889
rub <rubn@blo.fr>
2022-07-04 14:36:24 +0200

    modification pour que l'exercice soit pret

---
+++ README.md
@@ -0,0 +1,11 @@
+
+ # TP MODALS
+
+Vous allez réaliser trois modal dans votre site.  
+Les modals sont des fenêtre de type popup qui apparaissent pour donner des informations à vos utilisateurs.  
+pour les deux première vous allez utilisez le design que je vous fournis en regardant l'url suivante.  
+https://rubenavone.github.io/modals-javascript/  
+Le troisième devra être une modal pour la connexion/inscription à votre site.  
+Cherchez un design qui vous plait et integrer le comme les précedent.  
+N'hésiter pas à passer un peu de temp sur la recherche pour faire un design qui vous plait et qui vous met au défis.  
+ 
--- modal.js
+++ modal.js
@@ -1,3 +1,16 @@
+/***
+ * TP MODALS
+ * Vous allez réaliser trois modal dans votre site.
+ * Les modals sont des fenêtre de type popup qui apparaissent pour donner des informations à vos utilisateurs.
+ * pour les deux première vous allez utilisez le design que je vous fournis en regardant l'url suivante.
+ * https://rubenavone.github.io/modals-javascript/
+ * Le troisième devra être une modal pour la connexion/inscription à votre site.
+ * Cherchez un design qui vous plait et integrer le comme les précedent.
+ * N'hésiter pas à passer un peu de temp sur la recherche pour faire un design qui vous plait et qui vous met au défis.
+ * 
+ */
+
+
 //Selecteur qui servira pour toutes les modale
 let globalModalSelector = document.querySelector(".modal");
 let crossSelector = document.querySelectorAll(".cross")
